841a361f41b87de0a06a5a920f8e0ea4f8c3a126
umuts <umut.serifler@vestel.com.tr>
2016-10-06 14:30:01 +0300

    test5

--- src/ios/CDVrtspPlayer.m
+++ src/ios/CDVrtspPlayer.m
@@ -7,27 +7,30 @@
 - (void)showStreamPlayer:(NSString*)url
 {
     self.hasPendingOperation = YES;
-
-    self.streamViewController = [[UIViewController alloc] instantiateViewControllerWithIdentifier:@"ViewController" bundle:nil];
-
-	[self.streamViewController setStreamUrl:url];
-
-	[self.viewController presentViewController:self.streamViewController animated:YES completion:nil];
-	
+    
+    
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    
+    self.streamViewController = [sb instantiateViewControllerWithIdentifier:@"myViewController"];
+    
+    [self.streamViewController setStreamUrl:url];
+    
+    [self.viewController presentViewController:self.streamViewController animated:YES completion:nil];
+    
 }
 
 - (void)playRTSP:(CDVInvokedUrlCommand*) command
 {
-	CDVPluginResult* pluginResult = nil;
-	NSString* streamURL = [command.arguments objectAtIndex:0];
-	
-	if (streamURL != nil) {
-		[self showStreamPlayer:streamURL];
-		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-	} else {
-		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Arg was null"];
-	}
-	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    CDVPluginResult* pluginResult = nil;
+    NSString* streamURL = [command.arguments objectAtIndex:0];
+    
+    if (streamURL != nil) {
+        [self showStreamPlayer:streamURL];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Arg was null"];
+    }
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
