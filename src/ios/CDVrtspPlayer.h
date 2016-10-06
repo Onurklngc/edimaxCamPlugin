eec17996b5c10f7d0ed1164d6371329bf61864a1
Onurklngc <onurkalinagac@gmail.com>
2016-10-06 17:47:33 +0300

    -- renamed

--- src/ios/CDVrtspPlayer.h
+++ src/ios/CDVrtspPlayer.h
@@ -3,6 +3,7 @@
 #import <AVFoundation/AVFoundation.h>
 #import <CoreGraphics/CoreGraphics.h>
 #import <Foundation/Foundation.h>
+#import "ViewController.h"
 #import "MediaStreamingLib/ViewController.h"
 
 
