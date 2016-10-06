f51584f9c6f4ef44776ce00dd4c81f3d069daebf
OnurKalinagac <onurkalinagac@gmail.com>
2016-10-06 19:53:43 +0300

    Revert "del"
    This reverts commit 9c96a8c323a6c9c0f4ff1067dc0f77b8ff42c1b6.

--- src/ios/CDVrtspPlayer.m
+++ src/ios/CDVrtspPlayer.m
@@ -4,6 +4,20 @@
 
 @synthesize viewController;
 
+- (void)showStreamPlayer:(NSString*)url
+{
+    self.hasPendingOperation = YES;
+    
+    
+    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    
+    self.streamViewController = [sb instantiateViewControllerWithIdentifier:@"ViewController"];
+    
+    [self.streamViewController setStreamUrl:url];
+    
+    [self.viewController presentViewController:self.streamViewController animated:YES completion:nil];
+    
+}
 
 - (void)playRTSP:(CDVInvokedUrlCommand*) command
 {
@@ -11,6 +25,7 @@
     NSString* streamURL = [command.arguments objectAtIndex:0];
     
     if (streamURL != nil) {
+        [self showStreamPlayer:streamURL];
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Arg was null"];
