20fbb6883e2bfb2b0643b4474d7a6f1825c699ba
OnurKalinagac <onurkalinagac@gmail.com>
2016-10-06 19:53:48 +0300

    Revert "de2"
    This reverts commit 7ddcc6f62a5cb601843087f8f7ebb7a2bf099064.

--- src/ios/CDVrtspPlayer.h
+++ src/ios/CDVrtspPlayer.h
@@ -10,10 +10,13 @@
 @interface CDVrtspPlayer : CDVPlugin{
 
 }
-
+@property (nonatomic, retain) ViewController* streamViewController;
+@property (readwrite, assign) BOOL hasPendingOperation;
 @property (nonatomic, weak) CDVInvokedUrlCommand* lastCommand;
 
 - (void)playRTSP:(CDVInvokedUrlCommand*)command;
+
+- (void)showStreamPlayer:(NSString*)url;
 
 
 @end
