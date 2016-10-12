fd379d573d1bae237ac5395e64f230a4630d8c1d
OnurKalinagac <onurkalinagac@gmail.com>
2016-10-12 14:43:46 +0300

    90

--- src/ios/CDVrtspPlayer.h
+++ src/ios/CDVrtspPlayer.h
@@ -1,4 +1,8 @@
 #import <Cordova/CDV.h>
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <Foundation/Foundation.h>
 #import "lib/ViewController.h"
 
 @interface CDVrtspPlayer : CDVPlugin{
